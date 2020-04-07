5cd00bd46be8c911197ef47d977a9864e0553139
jonr <jrenteria003@ikasle.ehu.eus>
2020-04-07 21:18:25 +0200

    dialog interfazea sartu scriptean

--- menu.sh
+++ menu.sh
@@ -326,25 +326,30 @@ function irten()
 ############
 ### Main ###
 ############
-menukoaukera=0
-while test $menukoaukera -ne 12
+
+# hasieraketa balioa
+aukera=13
+
+while [ $aukera -ne 12 ]
 do
-	#Muestra el menu
-	echo -e "0 Desinstalatu"
-	echo -e "1 Apache instalatu"
-	echo -e "2 Apache gaitu eta testatu"
-	echo -e "3 Apache host birtuala sortu" 
-	echo -e "4 Apache host birtuala testatu" 
-	echo -e "5 PHP instalatu"
-	echo -e "6 PHP testatu"
-	echo -e "7 Python3 ingurune birtuala sortu"
-	echo -e "8 APPa instalatu"
-	echo -e "9 APPa testatu"
-	echo -e "10 Apache erroreak bistaratu"
-	echo -e "11 SSH saiakerak bistaratu"
-	echo -e "12 Irten"
-	read -p "Hautatu ezazu aukera bat: " menukoaukera
-	case $menukoaukera in
+	aukera="$(dialog --title "Web-zerbitzaria" \
+		--menu "\n Aukeratu ekintza" 20 70 20 \
+		0 "Desinstalatu dena" \
+		1 "Instalatu apache" \
+		2 "Apache testeatu" \
+		3 "VirtualHost-a sortu" \
+		4 "VirtualHost-a testeatu" \
+		5 "PHP instalatu" \
+		6 "PHP testeatu" \
+		7 "Py3VENV sortu" \
+		8 "APP-a instalatu" \
+		9 "APP-a bistaratu" \
+		10 "Apache log-ak" \
+		11 "SSH log-ak" \
+		12 "Irten" \
+		2>&1 1>&3)"
+
+	case $aukera in
 		0) desinstalatuDena;;
 		1) apacheInstalatu;;
 		2) apacheTestatu;;
@@ -360,7 +365,7 @@ do
 		12) irten;;
 		*) ;;
 	esac 
-done 
 
-echo "SCRIPT-aren amaiera"
+done
+
 exit 0
